f4a5f5f28a03f3d3b1ff233311ed488e8e56e3a0
이재성 <commonhm@gmail.com>
2024-11-09 14:48:36 +0900

    jwt secret fix

--- src/main/java/com/im/user/util/JwtUtil.java
+++ src/main/java/com/im/user/util/JwtUtil.java
@@ -3,7 +3,8 @@ package com.im.user.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Value;
+import jakarta.annotation.PostConstruct;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -12,10 +13,20 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    @Value("${jwt.secret}")
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 120;
+
+    private final Environment env;
     private String secretKey;
 
-    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
+    public JwtUtil(Environment env) {
+        this.env = env;
+    }
+
+    @PostConstruct
+    private void init() {
+        this.secretKey = env.getProperty("jwt.secret");
+        System.out.println("Loaded JWT Secret Key: " + secretKey); // 테스트용 출력, 이후 보안을 위해 제거
+    }
 
     public String generateToken(String userNumber, Map<String, Object> claims) {
         return Jwts.builder()
